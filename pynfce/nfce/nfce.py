51208d9063ad037f5becaf7ebfc7435be0406e02
Matheus Cardoso <matheus.mcas@gmail.com>
2019-03-06 22:17:56 -0300

    Create a nfce module to hold get_nfce function

---
+++ pynfce/nfce/nfce.py
@@ -0,0 +1,10 @@
+from requests_html import HTMLSession
+from ..states.states import BA, load_state_class
+
+
+def get_nfce(url, state=BA):
+    session = HTMLSession()
+    response = session.get(url)
+
+    state = load_state_class(state)
+    return state.get_nfce(response)
