3d3e5f50ff46ab52811155d5610fda130c7b937b
Matheus <matheus.mcas@gmail.com>
2019-04-06 22:10:57 -0300

    Add docs to get_nfce and load state before anything

--- pynfce/nfce/nfce.py
+++ pynfce/nfce/nfce.py
@@ -3,8 +3,20 @@ from ..states.states import BA, load_state_class
 
 
 def get_nfce(url, state=BA):
+    """
+    Busca no site da SEFAZ do estado os dados da nfce
+
+    Args:
+        url (str): url extraída da leitura do QR Code da NFCe
+        state (str): estado referente a URL
+
+    Returns:
+        nfce (obj)
+
+    """
+
+    state = load_state_class(state)
     session = HTMLSession()
     response = session.get(url)
 
-    state = load_state_class(state)
     return state(session).get_nfce(response)
